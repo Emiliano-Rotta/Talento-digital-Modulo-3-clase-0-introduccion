63b0d0f7c35326b0f974fad5f10ffede339d8e3f
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-23 22:52:59 -0300

    solucion

---
+++ assets/solucion.js
@@ -0,0 +1,148 @@
+const personas = [
+    {
+        nombre: "Juan",
+        edad: 30,
+        profesion: "Ingeniero",
+        tieneHijos: true,
+        hijos: ["Sofía", "Lucas"],
+        paisResidencia: "Argentina",
+        lugarFavorito: "Montaña"
+    },
+    {
+        nombre: "María",
+        edad: 25,
+        profesion: "Diseñadora",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Chile",
+        lugarFavorito: "Playa"
+    },
+    {
+        nombre: "Pedro",
+        edad: 40,
+        profesion: "Doctor",
+        tieneHijos: true,
+        hijos: ["Martín"],
+        paisResidencia: "México",
+        lugarFavorito: "Bosque"
+    },
+    {
+        nombre: "Ana",
+        edad: 35,
+        profesion: "Abogada",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "España",
+        lugarFavorito: "Museo"
+    },
+    {
+        nombre: "Carlos",
+        edad: 28,
+        profesion: "Profesor",
+        tieneHijos: true,
+        hijos: ["Juliana", "Emilio"],
+        paisResidencia: "Colombia",
+        lugarFavorito: "Cafetería"
+    },
+    {
+        nombre: "Laura",
+        edad: 32,
+        profesion: "Periodista",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Perú",
+        lugarFavorito: "Teatro"
+    },
+    {
+        nombre: "Andrés",
+        edad: 29,
+        profesion: "Arquitecto",
+        tieneHijos: true,
+        hijos: ["Valeria"],
+        paisResidencia: "Brasil",
+        lugarFavorito: "Parque"
+    },
+    {
+        nombre: "Sofía",
+        edad: 26,
+        profesion: "Médico Veterinario",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Uruguay",
+        lugarFavorito: "Librería"
+    },
+    {
+        nombre: "Mateo",
+        edad: 45,
+        profesion: "Chef",
+        tieneHijos: true,
+        hijos: ["Tomás", "Felipe", "Gabriel"],
+        paisResidencia: "Costa Rica",
+        lugarFavorito: "Restaurante"
+    },
+    {
+        nombre: "Lucía",
+        edad: 33,
+        profesion: "Científica",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Panamá",
+        lugarFavorito: "Laboratorio"
+    }
+];
+
+
+// TIENEN HASTA LAS 21.23 para hacer estos ejercicios
+// CUALQUIER DUDA PREGUNTEN
+
+
+
+// 1) Generar 10 alert que salude a cada uno de las personas
+// for (let i = 0; i < personas.length; i++) {
+//     alert("hola " + personas[i].nombre);
+// }
+
+// 2) mostrar en consola el promedio de edad.
+// var suma = 0
+// for (let i = 0; i < personas.length; i++) {
+//     suma = suma + personas[i].edad
+//     // suma += personas[i].edad
+// }
+// var promedio = suma/personas.length
+// console.log(promedio)
+
+
+
+// 3) Quiero saber si hay uno que tenga la profesion de Profesor, con un alert me diga si hay un profesor, y si no hay profesor que diga no hay profesor
+// let profesion = false
+// for (let i = 0; i < personas.length; i++) {
+//     if(personas[i].profesion === "Profesor"){
+//         profesion = true
+// }
+// }
+// if (profesion){
+//     alert("si hay un profesor");
+// }else{
+//     alert("no hay un profesor"); 
+// }
+
+// 4) Usando for y condicionales. cuantos tienen hijos
+// var contadorPadre = 0
+// for (let i = 0; i < personas.length; i++) {
+//     if (personas[i].tieneHijos) {
+//         // contadorPadre = contadorPadre + 1
+//         contadorPadre++
+//     }
+// }
+// console.log("el numero de padres es: " + contadorPadre)
+
+// 5) cuantos hijos hay
+
+var contadorHijos = 0
+for (let i = 0; i < personas.length; i++) {
+    if (personas[i].tieneHijos) {
+        contadorHijos = contadorHijos + personas[i].hijos.length
+        // contadorHijos += personas[i].hijos.length
+    }
+}
+console.log("la cantidad de hijos es: " + contadorHijos)
