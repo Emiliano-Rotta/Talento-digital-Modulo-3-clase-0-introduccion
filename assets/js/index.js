d03be7187e64cca18289812f92735f9c6c785a06
Emiliano Rotta <emilianorottaf@gmail.com>
2024-08-23 22:02:24 -0300

    firsat commits

---
+++ assets/js/index.js
@@ -0,0 +1,103 @@
+const personas = [
+    {
+        nombre: "Juan",
+        edad: 30,
+        profesion: "Ingeniero",
+        tieneHijos: true,
+        hijos: ["Sofía", "Lucas"],
+        paisResidencia: "Argentina",
+        lugarFavorito: "Montaña"
+    },
+    {
+        nombre: "María",
+        edad: 25,
+        profesion: "Diseñadora",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Chile",
+        lugarFavorito: "Playa"
+    },
+    {
+        nombre: "Pedro",
+        edad: 40,
+        profesion: "Doctor",
+        tieneHijos: true,
+        hijos: ["Martín"],
+        paisResidencia: "México",
+        lugarFavorito: "Bosque"
+    },
+    {
+        nombre: "Ana",
+        edad: 35,
+        profesion: "Abogada",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "España",
+        lugarFavorito: "Museo"
+    },
+    {
+        nombre: "Carlos",
+        edad: 28,
+        profesion: "Profesor",
+        tieneHijos: true,
+        hijos: ["Juliana", "Emilio"],
+        paisResidencia: "Colombia",
+        lugarFavorito: "Cafetería"
+    },
+    {
+        nombre: "Laura",
+        edad: 32,
+        profesion: "Periodista",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Perú",
+        lugarFavorito: "Teatro"
+    },
+    {
+        nombre: "Andrés",
+        edad: 29,
+        profesion: "Arquitecto",
+        tieneHijos: true,
+        hijos: ["Valeria"],
+        paisResidencia: "Brasil",
+        lugarFavorito: "Parque"
+    },
+    {
+        nombre: "Sofía",
+        edad: 26,
+        profesion: "Médico Veterinario",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Uruguay",
+        lugarFavorito: "Librería"
+    },
+    {
+        nombre: "Mateo",
+        edad: 45,
+        profesion: "Chef",
+        tieneHijos: true,
+        hijos: ["Tomás", "Felipe", "Gabriel"],
+        paisResidencia: "Costa Rica",
+        lugarFavorito: "Restaurante"
+    },
+    {
+        nombre: "Lucía",
+        edad: 33,
+        profesion: "Científica",
+        tieneHijos: false,
+        hijos: [],
+        paisResidencia: "Panamá",
+        lugarFavorito: "Laboratorio"
+    }
+];
+
+// 1) Generar 10 alert que salude a cada uno de las personas
+
+// 2) mostrar en consola el promedio de edad.
+
+// 3) Quiero saber si hay uno que tenga la profesion de Profesor, con un alert me diga si o no
+
+// 4) Usando for y condicionales. cuantos tienen hijos
+
+// 5) cuantos hijos hay
+
